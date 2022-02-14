4aaeed68cd1541a0a792a9378f978a43d89b0197
arifoglu <arifoglu2020@gmail.com>
2022-02-14 20:15:47 +0100

    first import-export

---
+++ week1_calculate/mathOperations.js
@@ -0,0 +1,19 @@
+const addition = (pnumber1,pnumber2)=>{
+    return pnumber1 + pnumber2 ;
+};
+const subtraction = (pnumber1,pnumber2)=>{
+    return pnumber1 - pnumber2 ;
+};
+const multiply = (pnumber1,pnumber2)=>{
+    return pnumber1 * pnumber2 ;
+};
+const divide = (pnumber1,pnumber2)=>{
+    return pnumber1 / pnumber2 ;
+};
+
+module.exports = {
+    addition,
+    subtraction,
+    multiply,
+    divide,
+};
---
+++ week1_calculate/mathOperationsApp.js
@@ -0,0 +1,7 @@
+const calculator = require("./mathOperations");
+
+
+console.log(calculator.addition(8,4));
+console.log(calculator.subtraction(8,4));
+console.log(calculator.multiply(8,4));
+console.log(calculator.divide(8,4));
