c6eea0edf5f7685bbe48049b03973751213970ce
arifoglu <arifoglu2020@gmail.com>
2022-02-17 14:24:24 +0100

    i have  done  some examples

---
+++ react_trials2/src/components/Content.js
@@ -0,0 +1,18 @@
+const Content = () => {
+
+    const handleNameChange = () => {
+        const names = ['HALLO', 'BONJOUR', 'GUTEN MORGEN'];
+        const int = Math.floor(Math.random() * 3);
+        return names[int] ;
+      }
+
+  return (
+    <main>
+        <p>
+            Hello {handleNameChange()}
+        </p>
+    </main>
+  )
+}
+
+export default Content
