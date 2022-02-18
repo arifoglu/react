ad9463f763ce559880958b5c8ea3abee2ffcda55
arifoglu <arifoglu2020@gmail.com>
2022-02-18 22:31:12 +0100

    i have tried with bootstrap somehthing

---
+++ react_trials3/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Buttons from './components/Button';
+import Numeros from './components/Card';
+
+
+
+function App() {
+  return (
+    <div>
+      
+      <Buttons />
+      <Numeros />
+  
+    </div>
+  );
+}
+
+export default App;
