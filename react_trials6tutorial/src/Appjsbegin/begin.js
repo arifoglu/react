607af618fbad996333f30d690ef014b24cddc8ee
arifoglu <arifoglu2020@gmail.com>
2022-03-04 22:51:42 +0100

    i have upload  somethings

---
+++ react_trials6tutorial/src/Appjsbegin/begin.js
@@ -0,0 +1,63 @@
+// import './App.css';
+
+// function App() {
+
+//   const title ='Welcome to new project';
+//   const likes=50;
+//  // const person ={ names : 'yoshi' , age :'34'};
+//  const link ="https://www.google.com" ;
+
+//   return (
+//     <div className="App">
+//       <div className='content'>
+//         <h1>{ title }</h1>
+//         <p>Liked {likes} times</p>
+
+//         {/* <p>{person}</p> */}
+
+//         <p>{10}</p>
+//         <p>{"hello"}</p>
+//         <p>{ [1,2,3,4,5] }</p>
+//         <p>{Math.random() * 10 }</p>
+
+//         <a href={ link } ></a>
+
+
+
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+//////State
+// import { useState } from "react";
+
+// const Home = () => {
+
+// //let name = 'mario';
+
+// const[name,setName]=useState('mario');
+// const[age,setAge]=useState(25)
+
+
+//     const handleClick = () => {
+//       setName('luigi');
+//       setAge(30);
+//     }
+
+
+
+//   return (
+//     <div className="home">
+//       <h2>Home Page</h2>
+//       <p>{ name } is {age} years old</p>
+//       <button onClick={handleClick}>Click me</button>
+ 
+
+//     </div>
+//   )
+// }
+
+// export default Home
---
+++ react_trials6tutorial/src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1> The New Project</h1>
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/create">News</a>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
